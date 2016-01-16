c0450904159b6fbab4131bdd878d0afa01298381
Ahmed Zain <tamkeenweb.com@gmail.com>
2016-01-16 15:29:58 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# electron-basic-updater
-A very basic updater for Electron apps
+# What it is
+> An NodeJs for Electron, that handles the application updates, in the most basic way.
+
+## How it works (Read this first)
+* EBU (Electron Basic Updater) was built to handle the process of updating an Electron app in the most basic way; **it simply replaces the application files (at /resources/app/) with the new ones representing the "update"!** 
+    > If this is what you are looking for then hope you like it, else please check out a more advanced tool to handle this, somthing like [electron-updater](https://www.npmjs.com/package/electron-updater). Or have a look at [this](http://electron.atom.io/docs/v0.33.0/api/auto-updater/).
+* The check for "updates" must by triggered by the application. **EBU doesn't make any kind of periodic checks on its own**. I suggest you use something like [node-schedule](https://www.npmjs.com/package/node-schedule) for this task.
+* EBU talks to an API (let's call it so) to tell it if there is an update pending installation.
+    * The API recieves a request from EBU with the client's **current version of the application (must be specified inside the application package.json file)**.
+    * The API then responds with the new update, ... or simply *false* to abort.
+    * If there's an update available the API should respond with the *source* for this update **.zip** file.
+    * EBU then downloads the .zip file and - I am sorry for the following - extracts it directly to the application folder (/resources/app/), thus overwriting the current files, including itself!
+
+## But why ? (use cases)
+Well, right now I am working on an Electron application for me and my collegues to use at work, after finishing the application and giving them the .exe release I needed to change couple of things, so I asked myself: what to do now ?! I read [this](http://electron.atom.io/docs/v0.33.0/api/auto-updater/) but for the life of me I couldn't get it to work :( (but to be fair, I didn't give it enough time, I wanted to create this instead :) ). So I decided to create a module to handle this, in the most basic way possible. So ...
+* You can use EBU when you are still building the application and need a group of people to live-test it on their machines with you monitoring the process and pushing changes.
+* Also if your application is a local thing, inside your company or work group, and you simply need to push "changes" to their copies.
+* If you don't know how to get the [Squirrel](https://github.com/Squirrel) thing to work and need to roll out your application asap.
+---
+## Installation
+```
+    $ npm install --save electron-basic-updater
+```
+Now, inside the *main.js* file, call it like this:
+```
+    const Electron = require('electron');
+    const Application = Electron.app;
+    const EBU = require('electron-basic-updater');
+    
+    Application.on('ready', function(){
+        // Initiate the module
+        EBU.init({
+            'api': 'http:// .... ' // The API EBU will talk to
+        });
+    });
+```
+That's it. Now, you can use ```EBU.check()``` to trigger the update process; EBU will first check for updates, if there was a new update, EBU will download it and extract it to the application folder.
+
+---
+# API
+## `Init( setup )`
+* **setup** (object) The module setup
+    * **api** (string) The URL EBU will call to check for updates.
+    * **logFile** (string) [optional] The file to log the update process updates and errors to it, pass *FALSE* to omit logging . Defaults to "updater-log.txt".
+    * **requestOptions** (object) [optional] The global options for the HTTP requests EBU will make to check for updates and download them. EBU uses the cool [restler](https://github.com/danwrong/restler) for these requests, and the `requestOptions` will be used for all the requests ([the full options list](https://github.com/danwrong/restler#options)). You can use this option to pass an `accessToken` or a `username` and `password` along with the request, or even send some extra data along with the request through the `data` property.
+        ```
+            EBU.init({
+                'api': 'http:// ...',
+                'requestOptions': {'accessToken': ..., 'data': {'group': 'friends'}}
+            });    
+        ```
+## `check()`
+Will check for an update, if an update was found, will download it and install it! As mentioned, this method must be tirggerd, EBU wont check for updates on its own.
+
+---
+# The update server
+And I mean this in the most simple way possible. This server/API will recieve one request, which is the check for updates, and will send one response of :
+* **New update:** `{"last": " [the new version] ", "source": " [the .zip file url] "}` **EBU wont make any version comparsions, it will simply download the `source` url and extract it**. So, you will need to handle this on your side, EBU sends (POST-type request) you the client's current version (as `current`), you can use this to send the correct update!
+* **Any other value, to cancel the update**
+---
+* Please contact me with any comments or open an issue.
+* The development of this module will be continued.
+---
+The MIT License (MIT) - 
+Copyright (c) 2015 Ahmed Zain tamkeenlms@gmail.com
---
+++ index.js
@@ -0,0 +1,190 @@
+
+    /**
+     * Hi there,
+     * Thanks for using this module, and good luck with your Electron app.
+     *
+     * ~ Zain
+     * */
+
+    const Electron = require('electron');
+    const Application = Electron.app;
+    const FileSystem = require('fs');
+    const Utils = require('util');
+    const Zip = require('adm-zip');
+    const HTTP = require('restler');
+    const Path = require('path');
+    const AppPath = Application.getAppPath() + Path.sep;
+
+    /**
+     * */
+    var Updater = {
+        /**
+         * The setup
+         * */
+        'setup': {
+            'api': null,
+            'logFile': 'updater-log.txt',
+            'requestOptions': {}
+        },
+
+        /**
+         * The new update information
+         * */
+        'update': {'last': null, 'source': null, 'file': null},
+
+        /**
+         * Init the module
+         * */
+        'init': function(setup){
+            if(!Utils.isObject(setup)){
+                this.log('Invalid setup!');
+            }
+
+            this.setup = Utils._extend(this.setup, setup);
+        },
+
+        /**
+         * Logging
+         * */
+        'log': function(line){
+            // Log it
+            Utils.log('Updater: ', line);
+
+            // Put it into a file
+            if(this.setup.logFile){
+                FileSystem.appendFileSync(this.setup.logFile, line + "\n");
+            }
+        },
+
+        /**
+         * Make the check for the update
+         * */
+        'check': function(){
+            // Get the current version
+            var packageInfo = require(AppPath + '/package.json');
+
+            // If the version property not specified
+            if(!packageInfo.version){
+                this.log('The "version" property not defined inside the application package.json');
+                return false;
+            }
+
+            var requestOptions = Utils._extend({}, this.setup.requestOptions);
+            if(!requestOptions.data){
+                requestOptions.data = {};
+            }
+
+            // Send the current version along with the request
+            requestOptions.data.current = packageInfo.version;
+
+            // Check for updates
+            HTTP.post(this.setup.api, requestOptions)
+                .on('complete', function(result){
+                    // If the request failed
+                    if(result instanceof Error){
+                        Updater.log('Could not connect, ' + result.message);
+                        return false;
+                    }
+
+                    // Connected!
+                    Updater.log('Connected to ' + Updater.setup.api);
+
+                    // Handle the response
+                    try{
+                        if(!result){
+                            throw false;
+                        }
+
+                        // Parse the response
+                        var response = JSON.parse(result);
+
+                        // If the "last" property is not defined
+                        if(!response.last){
+                            throw false;
+                        }
+
+                        // Update available
+                        if(response.source){
+                            Updater.log('Update available: ' + response.last);
+
+                            // Store the response
+                            Updater.update = response;
+
+                            // Download the update
+                            Updater.download();
+
+                        }else{
+                            Updater.log('No updates available');
+                            return false;
+                        }
+
+
+                    }catch(error){
+                        Updater.log('API response is not valid');
+                    }
+                });
+        },
+
+        /**
+         * Download the update file
+         * */
+        'download': function(){
+            var url = this.update.source,
+                fileName = 'update.zip';
+
+            this.log('Downloading ' + url);
+
+            var requestOptions = Utils._extend({}, this.setup.requestOptions);
+            requestOptions.decoding = 'buffer';
+
+            // Download the file
+            HTTP.get(url, requestOptions)
+                .on('complete', function(data){
+                    // The request failed
+                    if(data instanceof Error){
+                        Updater.log('Could not find the update file.');
+                        return false;
+                    }
+
+                    // The file full path
+                    var updateFile = AppPath + fileName;
+
+                    // Create the file
+                    FileSystem.writeFile(updateFile, data, null, function(error){
+                        if(error){
+                            Updater.log('Failed to download the update to a local file.');
+                            return false;
+                        }
+
+                        // Store the update file path
+                        Updater.update.file = updateFile;
+
+                        // Success
+                        Updater.log('Update downloaded: ' + updateFile);
+
+                        // Apply the update
+                        Updater.apply();
+                    });
+                });
+        },
+
+        /**
+         * Apply the update, it simply overwrites the current files!
+         * */
+        'apply': function(){
+            try{
+                this.log('Extracting the new update files.');
+
+                var zip = new Zip(this.update.file);
+                zip.extractAllTo(AppPath);
+
+                this.log('New update files were extracted.');
+                this.log('End of update.');
+
+            }catch(error){
+                this.log('Extraction error: ' + error);
+            }
+        }
+    };
+
+    module.exports = Updater;
